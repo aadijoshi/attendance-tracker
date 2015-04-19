6eb015d9938f8f390bf4c4df8c61e1c511f1e4cf
moiri-gamboni <moiri.gamboni@nyu.edu>
2015-04-19 21:25:49 +0400

    readded index.ios.js

--- AttendanceTracker/index.ios.js
+++ AttendanceTracker/index.ios.js
@@ -2,52 +2,179 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  */
-'use strict';
+ 'use strict';
 
 var React = require('react-native');
-var {
+
+ var {
   AppRegistry,
+  DatePickerIOS,
+  TouchableHighlight,
+  TabBarIOS,
+  ListView,
+  TextInput,
   StyleSheet,
   Text,
   View,
 } = React;
 
-var AttendanceTracker = React.createClass({
+
+
+
+//var CardReaderManager = require('NativeModules').CardReaderManager;
+//
+//CardReaderManager.test((error, msg) => {
+//  if (error) {
+//    console.error(error);
+//  } else {
+//    console.log(msg);
+//  }
+//});
+var TabBarIOSItemContent = React.createClass({
+  _icon: function(imageUri) {
+    return {
+      uri: imageUri,
+      isStatic: true
+    };
+  },
+  onPressHandler: function() {
+    this.props.onPressHandler(this.props.tabName);
+  },
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+      <TabBarIOS.Item
+        icon={this._icon(this.props.iconURI)}
+        selected={this.props.selectedTab === this.props.tabName}
+        onPress={this.onPressHandler}
+      >
+        {this.props.children}
+      </TabBarIOS.Item>
+    );
+  },
+});
+
+var AttendanceTracker = React.createClass({
+
+  getInitialState: function() {
+    return {
+      selectedTab: 'createTab',
+    };
+  },
+
+  onPressHandler: function(tabName) {
+    this.setState({
+      selectedTab:tabName,
+    });
+    console.log(tabName);
+  },
+
+  render: function() {
+    return (
+      <TabBarIOS>
+        <TabBarIOSItemContent tabName='createTab' iconURI='favorites' selectedTab={this.state.selectedTab} onPressHandler={this.onPressHandler}>
+            <Event title="Create a New Event"></Event>
+        </TabBarIOSItemContent>
+        <TabBarIOSItemContent tabName='editTab' iconURI='history' selectedTab={this.state.selectedTab} onPressHandler={this.onPressHandler}>
+            <Event title="Edit an Existing Event"></Event>
+        </TabBarIOSItemContent>
+        <TabBarIOSItemContent tabName='attendanceTab' iconURI='more' selectedTab={this.state.selectedTab} onPressHandler={this.onPressHandler}>
+            <Search title="Find an Event to Start Taking Attendance"></Search>
+        </TabBarIOSItemContent>
+        <TabBarIOSItemContent tabName='syncTab' iconURI='contacts' selectedTab={this.state.selectedTab} onPressHandler={this.onPressHandler}>
+            <View style={styles.container}><Text>bla4</Text></View>
+        </TabBarIOSItemContent>
+      </TabBarIOS>
+    );
+  },
+});
+
+var Event = React.createClass({
+  getDefaultProps: function() {
+    return {
+      date: new Date(),
+    };
+  },
+  getInitialState: function() {
+    return {
+      date: this.props.date,
+    };
+  },
+  render: function() {
+    return (
+       <View style={styles.container}>
+        <Text style={styles.title}>
+          {this.props.title}
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+Control+Z for dev menu
-        </Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            autoFocus={false}
+            clearButtonMode='while-editing'
+            onChangeText={(text) => this.setState({eventName: text})}
+            placeholder='Event Name'
+            returnKeyType='next'
+            keyboardType='default'
+          />
+          <DatePickerIOS
+            date={this.state.date}
+            mode="date"
+            onDateChange={(date) => this.setState({date: date})}
+          />
+        </View>
+        <TouchableHighlight style={styles.submit} onPress={function(){console.log(this.state.date)}}>
+          <Text>Submit</Text>
+        </TouchableHighlight>
       </View>
     );
-  }
+  },
 });
+
+var Event = React.createClass({
+  render: function() {
+    return (
+       <View style={styles.container}>
+        <Text style={styles.title}>
+          {this.props.title}
+        </Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            autoFocus={false}
+            clearButtonMode='while-editing'
+            onChangeText={(text) => this.setState({eventName: text})}
+            placeholder='Event Name'
+            returnKeyType='next'
+            keyboardType='default'
+          />
+          <DatePickerIOS
+            date={this.state.date}
+            mode="date"
+            onDateChange={(date) => this.setState({date: date})}
+          />
+        </View>
+        <TouchableHighlight style={styles.submit} onPress={function(){console.log('submit')}}>
+          <Text>Submit</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  },
+});
+
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    alignItems: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+  }
 });
 
 AppRegistry.registerComponent('AttendanceTracker', () => AttendanceTracker);
+
+
