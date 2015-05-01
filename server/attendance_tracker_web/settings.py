f68823fbbc1317c3021d544615d340c4d1ae67cf
Oleg Grishin <og402@nyu.edu>
2015-05-01 18:52:05 +0400

    Server settings

--- server/attendance_tracker_web/settings.py
+++ server/attendance_tracker_web/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'attendance_tracker_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'attendance',
+        'USER': 'ios',
+        'PASSWORD': 'ios123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
