5f9c4508208cb008d30a3b5163755f1fc1796ad5
Oleg Grishin <og402@nyu.edu>
2015-05-06 01:55:12 +0400

    Added date checking; added modal clearing; added custom semester

--- server/static/js/index/script.js
+++ server/static/js/index/script.js
@@ -9,17 +9,22 @@ $(function(){
         return loadingElement(dict);
     }
 
+    var glyphiconElement = _.template("<span class='glyphicon <%= type %>'></span>");
+
 
     // To smoothen loading put intervals (or put to 0 for fastest speed)
     var loadingInterval = 1000;
     // To store dates of semesters globally
     var semesters, events;
+    // Date format (if changes, need to be changed in several places)
+    var dateFormat = "YYYY-MM-DD";
 
     // Add delay to display the modal
     $('#loading').on('show.bs.modal', function (e) {
         setTimeout(function(){
             loadSemesters();
         }, loadingInterval);
+        $('#loading').off('show.bs.modal');
     });
 
     // Load semesters data from the server
@@ -59,13 +64,22 @@ $(function(){
                         name : semester.fields.term + " " + semester.fields.year
                     }));
                 });
+                $("#semesterInput").append(selectSemesterElement({
+                    value : -1,
+                    name : "custom"
+                }));
                 $("#semesterInput").on("change", function(val) {
                     var val = $("#semesterInput :selected").val();
-                    var semester = _.find(semesters, function(semester) {
-                        return semester.pk == val;
-                    });
-                    $("#startDatePicker").val(semester.fields.start_date);
-                    $("#endDatePicker").val(semester.fields.end_date);
+                    if (val != -1) {
+                        var semester = _.find(semesters, function(semester) {
+                            return semester.pk == val;
+                        });
+                        $("#startDatePicker").val(semester.fields.start_date);
+                        $("#endDatePicker").val(semester.fields.end_date);
+                    } else {
+                        $("#startDatePicker").focus();
+                    }
+
                 });
                 try {
                     current = JSON.parse(current);
@@ -90,14 +104,25 @@ $(function(){
     // Loads events from the server
     var loadEvents = function () {
         var start_date = $("#startDatePicker").val();
+        // check if start date is valid
+        if (!moment(start_date, dateFormat, true).isValid()) {
+            dismissibleError("Wrong start date format, please use " + dateFormat);
+            return;
+        }
         var end_date = $("#endDatePicker").val();
+        // check if end date is valid
+        if (!moment(end_date, dateFormat, true).isValid()) {
+            dismissibleError("Wrong end date format, please use " + dateFormat);
+            return;
+        }
+
         $.getJSON("events/json", {
             start : start_date,
             end : end_date
         })
         .always(function() {
             $("#loading .modal-body").append(getLoadingElement({
-                message : "Loading events info for current semester&hellip;",
+                message : "Loading events info for selected date range&hellip;",
                 type : "text-info"
             }));
         })
@@ -129,7 +154,37 @@ $(function(){
         setTimeout(function(){
             $('#loading').modal('hide');
         }, loadingInterval);
+    }
 
+    var resetModal = function (dict) {
+        // _.defaults cannot do defaults on undefined
+        $("#loading .modal-body").html("");
+        var to_append_after = "";
+
+        if (!dict) {
+            dict = {};
+            to_append_after = "<h4>Loading, please wait&hellip;</h4>";
+        }
+        _.defaults(dict, {type : "glyphicon-refresh glyphicon-refresh-animate"});
+
+        $("#loading .modal-body").append(glyphiconElement(dict));
+
+        $("#loading .modal-body").append(to_append_after);
+    };
+
+    var dismissibleError = function (errorText) {
+
+        resetModal({type : "glyphicon-exclamation-sign"});
+
+        $("#loading .modal-body").append(getLoadingElement({
+            h_num : 4,
+            message : errorText,
+            type : "text-danger"
+        }));
+
+        setTimeout(function(){
+            $('#loading').modal('hide');
+        }, loadingInterval);
     }
 
     // Adds error message to the loading modal
@@ -142,6 +197,27 @@ $(function(){
         }));
         $("#loading .modal-body .glyphicon").removeClass("glyphicon-refresh-animate glyphicon-refresh").addClass("glyphicon-remove");
     }
+
+    // Event binders
+
+    // Reload on click
+    $("#applyDateRange").on("click", function(e) {
+        e.preventDefault();
+        resetModal();
+        $('#loading').modal('show');
+
+        setTimeout(function(){
+            loadEvents();
+        }, loadingInterval);
+    });
+
+    // Remove semester on change
+    $("#startDatePicker").on("focus", function() {
+        $("#semesterInput").val(-1);
+    });
+    $("#endDatePicker").on("focus", function() {
+        $("#semesterInput").val(-1);
+    });
 
     $('#loading').modal('show');
 
